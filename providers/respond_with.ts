847a84292e386d6a5d802348e8a9df7d2a13135e
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2025-02-11 19:11:04 +0100

    Switch to preserve module for typescript

--- providers/respond_with.ts
+++ providers/respond_with.ts
@@ -1,11 +1,11 @@
 import { ApplicationService } from '@adonisjs/core/types'
-import { RespondWithOptions } from '../src/types.js'
+import { RespondWithOptions } from '../src/types'
 
 export default class RespondWithProvider {
   constructor(protected app: ApplicationService) {}
 
   async boot() {
-    await import('../src/extension.js')
+    await import('../src/extension')
   }
 
   register() {
